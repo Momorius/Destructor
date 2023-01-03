c1045ef46390cd566fed37a00c8f063d2ba41746
Morius <alejo.lopezbluma@gmail.com>
2023-01-03 17:51:14 -0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace FileDamageExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // El nombre del archivo a dañar
+            Console.WriteLine("Version 1.0");
+            string fileName = "example.txt";
+
+            // Creamos una copia de seguridad del archivo original
+            string backupFileName = $"{fileName}.backup";
+            File.Copy(fileName, backupFileName, true);
+
+            // Abrimos el archivo en modo lectura y escritura
+            using (FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite))
+            {
+                // Cambiamos el contenido del archivo de manera aleatoria
+                Random rnd = new Random();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    for (int i = 0; i < bytesRead; i++)
+                    {
+                        // Generamos un nuevo valor aleatorio para el byte
+                        buffer[i] = (byte)rnd.Next(256);
+                    }
+
+                    // Escribimos los bytes aleatorios en el archivo
+                    stream.Position -= bytesRead;
+                    stream.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
